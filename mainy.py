03d2097144daf204f0135964f7170f4e09131bcb
RVP97 <85131345+RVP97@users.noreply.github.com>
2022-06-28 18:22:38 -0500

    Add files via upload

--- mainy.py
+++ mainy.py
@@ -1,22 +1,28 @@
 from pathlib import Path
 
-folder = Path(r"C:\Users\52553\Downloads")
-for file in folder.rglob('*'):
-    if file.is_file():
-        suffix = file.suffix.replace('.', '').upper()
-        if not Path(folder / suffix).exists():
-            new_folder = Path(folder / suffix)
-            new_folder.mkdir()
-        try:
-            file.rename(folder / suffix / file.name)
-        except FileExistsError:
-            file.unlink()
+# List of folders to organize into folders by filetype
+folders = [
+    r"C:\Users\52553\OneDrive\Documents",
+    r"C:\Users\52553\Downloads"
+]
 
-for x in range(10):
-    for file in folder.rglob('**/*'):
-        if file.is_dir():
+for folder in folders:
+    folder = Path(folder)  # creates a path object from the string
+    for file in folder.rglob('*'):  # iterates through all files in the folder
+        if file.is_file():  # checks if the file is a file
+            suffix = file.suffix.replace('.', '').upper()  # removes the dot from the file extension and makes it upper
+            if not Path(folder / suffix).exists():  # checks if the folder does not exist yet
+                new_folder = Path(folder / suffix)
+                new_folder.mkdir()  # creates folder with new name
             try:
-                file.rmdir()
-            except:
-                pass
+                file.rename(folder / suffix / file.name)  # tries to move file to new folder
+            except FileExistsError:  # if file already exists in new folder
+                file.unlink()  # deletes file
 
+    for x in range(10):
+        for file in folder.rglob('**/*'):  # iterates through all files in the folder
+            if file.is_dir():  # checks if the file is a directory
+                try:
+                    file.rmdir()  # tries to delete directory
+                except:
+                    pass  # if directory is not empty, it will not be deleted
